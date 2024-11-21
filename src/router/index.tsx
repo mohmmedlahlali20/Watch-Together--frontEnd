de19e85111ac0d5b98211a1ca63dd5e882dd4b43
mohmmedlahlali20 <mohmmedlaeh81@gmail.com>
2024-11-21 14:24:26 +0100

    Import register and login components and create router for it

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,17 +1,22 @@
 import { createBrowserRouter } from "react-router-dom";
 import { Layout } from "../layout";
+import { Login, Register } from "../components";
 
-const router = createBrowserRouter(
-    [
-        {
-            path: "/",
-            element: <Layout />,
-            children: [
-
-            ],
-        },
-    ],
-
-);
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <Layout />,
+        children: [
+        ],
+    },
+    {
+        path: "/login",
+        element: <Login />,
+    },
+    {
+        path: "/register",
+        element: <Register />,
+    },
+]);
 
 export default router;
