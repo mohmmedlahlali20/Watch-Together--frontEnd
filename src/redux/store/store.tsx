80514db1cc5d3fc21200defb3912aa64383d4e70
mohmmedlahlali20 <mohmmedlaeh81@gmail.com>
2024-11-22 21:52:18 +0100

    Import : import Users  Reducer

--- src/redux/store/store.tsx
+++ src/redux/store/store.tsx
@@ -1,11 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit';
 import authReducer from '../slice/auth/authSlice';
 import roomReducer from '../slice/room/roomSlice';
+import userReducer from '../slice/users/usersSlice.ts';
 
 export const store = configureStore({
     reducer: {
         auth: authReducer,
         room: roomReducer,
+        user: userReducer
     },
 });
 
