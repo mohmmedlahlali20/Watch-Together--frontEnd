05c100acfef8d06e3c7268d5e46cd02658474e46
mohmmedlahlali20 <mohmmedlaeh81@gmail.com>
2024-11-22 21:47:15 +0100

    Import : CreateRoom Components

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,8 +1,10 @@
 import Login from "./auth/Login.tsx";
 import Register from "./auth/Register.tsx";
+import CreateRoom from "./rooms/CreateRoom.tsx";
 
 
 export {
     Login,
     Register,
+    CreateRoom,
 }
