4854fb560fb748938c9836c61f2377e8d0a255f7
mohmmedlahlali20 <mohmmedlaeh81@gmail.com>
2024-11-22 06:32:28 +0100

    import store from redux

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,12 @@
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { store } from './redux/store/store';
+import App from './App';
 
-createRoot(document.getElementById('root')!).render(
-    <App />
-
-)
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>,
+    document.getElementById('root')
+);
