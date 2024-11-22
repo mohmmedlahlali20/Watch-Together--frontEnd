a956a43ec69f0b576fa0aef373b416224132d758
mohmmedlahlali20 <mohmmedlaeh81@gmail.com>
2024-11-22 21:48:34 +0100

    Import : import Room Reducer

--- src/redux/store/store.tsx
+++ src/redux/store/store.tsx
@@ -1,11 +1,14 @@
 import { configureStore } from '@reduxjs/toolkit';
-import authReducer from "../slice/auth/authSlice.ts";
+import authReducer from '../slice/auth/authSlice';
+import roomReducer from '../slice/room/roomSlice';
 
 export const store = configureStore({
     reducer: {
         auth: authReducer,
+        room: roomReducer,
     },
 });
+
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
