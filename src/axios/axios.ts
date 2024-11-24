b5dd0636d6891411b1d329652dc0c558f63fe584
mohmmedlahlali20 <mohmmedlaeh81@gmail.com>
2024-11-24 14:40:18 +0100

    Add : Add authorization by token in config function

--- src/axios/axios.ts
+++ src/axios/axios.ts
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import Cookies from "js-cookie";
 
 const path = axios.create({
     baseURL: import.meta.env.VITE_NEST_API_URL,
@@ -8,6 +9,11 @@ path.interceptors.request.use(
     function(config) {
         config.headers['Content-Type'] = 'multipart/form-data';
         config.headers['Content-Type'] = 'application/json';
+        const token = Cookies.get('token')
+
+        if (token) {
+            config.headers['Authorization'] = `Bearer ${token}`;
+        }
         return config;
     },
     function(err) {
